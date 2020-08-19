f7855facac1a243c063e3dc6a454206d38e3386e
wanghou5 <yuwa0904@colorado.edu>
2020-08-19 21:35:22 +0800

    90 percent complete
    90 percent complete

---
+++ Assets/scripts/PrintAndHide.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class PrintAndHide : MonoBehaviour
+{
+    // Start is called before the first frame update
+       int i = 0;
+    public Renderer rend;
+    void Start()
+    {
+         
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UnityEngine.Debug.Log("<"+gameObject.name+">" + ":"+ i);
+        i++;
+    }
+}

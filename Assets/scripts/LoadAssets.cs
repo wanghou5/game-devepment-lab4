547f37346351188c0be6000dca1991682eda172b
wanghou5 <yuwa0904@colorado.edu>
2020-08-19 20:57:56 +0800

    70 percent complete
    70 percent complete

--- Assets/scripts/LoadAssets.cs
+++ Assets/scripts/LoadAssets.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using UnityEngine;
 
 public class LoadAssets : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject redObj;
+    [SerializeField] private GameObject blueObj;
     void Start()
     {
-        
+        Instantiate(redObj, new Vector3(2, 0, 0), Quaternion.identity);
+        Instantiate(blueObj, new Vector3(-2, 0, 0), Quaternion.identity);
     }
 
     // Update is called once per frame
